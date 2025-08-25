46b2916c8818453a53703ca19d30b47670edde46
Arthur <arthur.rodriguessil21@gmail.com>
2025-08-25 20:38:24 -0300

    feat: Adiciona a pagina de detalhes

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -4,15 +4,21 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Home.vue') },
-      
+
       {
         path: 'teste-back',
         component: () => import('src/pages/TesteConexaoBack.vue')
-      }
+      },
+
+      {
+    path: 'details/:id',   
+    component: () => import('src/pages/EventoDetails.vue'),
+    props: true
+    }
+
     ]
   },
 
-  
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
