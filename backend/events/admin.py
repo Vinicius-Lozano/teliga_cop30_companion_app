671b83bca225e52e624d4bf4dd94d72484de1fa6
Arthur <arthur.rodriguessil21@gmail.com>
2025-08-31 17:25:40 -0300

    atualização backend eventos

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Evento
 
-# Register your models here.
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "descricao", "latitude", "longitude", "data", "icone")
+    search_fields = ("titulo", "descricao")
+    list_filter = ("data",)
