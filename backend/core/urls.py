12fc98997fd71367d26471ededd6fcfbd5498722
Fernando <nando.nicolau27@gmail.com>
2025-09-01 15:47:56 -0300

    CRUD DE EVENTOS(ATT)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,16 @@
 """
 URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+
+# Imports necessários para servir arquivos de mídia em desenvolvimento
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.views.generic.base import RedirectView
@@ -16,9 +20,9 @@ def teste_rota(request):
     return JsonResponse({"status": "ok", "message": "Conexão com o backend bem-sucedida!"})
 
 urlpatterns = [
-    
+
     path('', RedirectView.as_view(url='/api/swagger/', permanent=False)),
-    
+
     path('admin/', admin.site.urls),
 
     # Rotas de Autenticação (Login, Logout, Registro, etc.)
@@ -32,10 +36,13 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # APPS locais
-    path('api/', include('users.urls')),
-    path('api/', include('events.urls')),
-    path('api/', include('item.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/events/', include('events.urls')),
 
     # teste rota
     path('api/teste_rota_back/', teste_rota, name='teste_rota_back'),
 ]
+
+# Adiciona a rota para servir arquivos de mídia quando DEBUG=True
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
